8d894624d3858755ab9bd18e91f595554496743a
Steve Calla <callasteven@gmail.com>
2022-07-22 14:31:47 -0600

    Add jquery autocomplete function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,64 @@
 //section:query selector variables go here 👇
+let citySelectionButton = document.getElementById('button-wrapper');
+let cityInputSearchText = document.getElementById('city-search-input-text');
+// let citySelected = document.getElementById('citySelected').textContent;
 
 //section:global variables go here 👇
 
 //section:event listeners go here 👇
+citySelectionButton.addEventListener('click', getWeatherData);
+// cityInputSearchText.addEventListener('input', function() {
+//   console.log(cityInputSearchText.value)
+// })
 
 //section:functions and event handlers go here 👇
-console.log('hello')
+
+function getWeatherData(event) {
+  //get city selected
+  if (event.target.matches('button')) {
+    let selectedCity = getcitySelected(event);
+    //fetch current weather
+    let currentWeather = getCurrentWeather(selectedCity);
+    //fetch forecast
+    //render current weather
+    // console.log(currentWeather);
+    //render forecast
+    //clear current weather & forecast on next request
+  }
+}
+
+// GET CITY INPUT
+
+function getcitySelected(event) {
+  let citySelected = event.target.textContent.trim().toLowerCase();
+  citySelected === 'search' ? citySelected = cityInputSearchText.value.trim().toLowerCase() : citySelected;
+
+  citiesUSOnly.filter(element => {
+    // console.log(citySelected, element.name, citySelected === element.name);
+    if (citySelected === element.name.toLowerCase()) {console.log(citySelected, element.name.toLowerCase(), element.state, citySelected === element.name.toLowerCase());};
+    // console.log(element.name, element.state, element.coord.lon, element.coord.lat)
+  })
 
 
 
+  return citySelected;
+}
 
+// RENDER WEATHER DATA
+function renderWeather(data, currentOrFuture) {
+  console.log(data, currentOrFuture);
+  console.log(data.dt, data.name, data.weather[0].description, data.weather[0].icon, data.main.temp, data.main.humidity, data.wind.speed)
+}
+
+// WEATHER API CALLS TO OPENWEATHER
 function getCurrentWeather(cityName) {
-  let currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},co,us&APPID=f0bed1b0eff80d425a392e66c50eb063&units=imperial`;
-  fetch(currentWeatherURL)
-  .then(response => response.json())
-  .then(data => console.log(data));
+  // console.log('api call = ', cityName)
+  // let currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},co,us&APPID=f0bed1b0eff80d425a392e66c50eb063&units=imperial`;
+  // fetch(currentWeatherURL)
+  // .then(response => response.json())
+  // .then(data => renderWeather(data, 'current'));
+
+  return renderWeather(currentWeather);
 }
 
 function getForecastWeather(cityName) {
@@ -24,5 +68,9 @@ function getForecastWeather(cityName) {
   .then(data => console.log(data));
 }
 
+
+// CREATE LIST OF CITY/STATES
+// let nameState = citiesUSOnly.map(({name, state}) => (`${name}, ${state}`),[]);
+// console.log(nameState);
 
 
